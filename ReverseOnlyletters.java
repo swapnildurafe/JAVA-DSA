b31575f21551edfb68fe9aa0dd6f3a0229d7c054
swapnildurafe <142746612+swapnildurafe@users.noreply.github.com>
2025-09-18 23:05:34 +0530

    Add files via upload

---
+++ ReverseOnlyletters.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class ReverseOnlyletters{
+    public static String rev(String s){
+        int start = 0;
+        int end = s.length()-1;
+        char[] ch = s.toCharArray();
+        while(start<end){
+            if(!Character.isLetter(ch[start])){
+                start++;
+            }
+            else if(!Character.isLetter(ch[end])){
+                end--;
+            }
+            else{
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        return new String(ch);
+    }
+
+    public static void main(String[] args){
+        String s = "ab-cd";
+        System.out.print(rev(s));
+    }
+}
